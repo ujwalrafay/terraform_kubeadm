5281e0bc5236479610db9c0ddc825e950a4ac584
ujwalrafay <102654760+ujwalrafay@users.noreply.github.com>
2022-05-04 13:23:19 +0530

    vpc edited

--- kubernetes/vpc.tf
+++ kubernetes/vpc.tf
@@ -1,13 +1,19 @@
-resource "aws_vpc" "main" {
-  cidr_block                       = var.vpc_cidr_block
+resource "aws_vpc" "uj-managed-vpc-terraform" {
+  cidr_block                       = "10.0.0.0/16"
   enable_dns_hostnames             = true
   enable_dns_support               = true
+  instance_tenancy = "default"
   tags = {
-    Name = var.vpc_name
+    Name = "uj-managed-vpc-terraform"
   }
 }
 
 resource "aws_vpc_dhcp_options" "dhcpos" {
-  domain_name         = "${var.region}.compute.internal"
-  domain_name_servers = ["AmazonProvidedDNS"]
+   vpc_id                  = aws_vpc.prod-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true" //it makes this a public subnet
+  availability_zone       = "us-west-2a"
+  tags = {
+    Name = "prod-subnet-public-1"
+  }
 }
